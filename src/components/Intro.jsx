e1af10a999a238b36efb1bbeb4c79193b2214768
morganebdlx <mlacroix8@gmail.com>
2025-05-27 11:59:41 +0200

    nouveau design, nouvelles sections, nouveau menu

---
+++ src/components/Intro.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Intro.css';
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div className="intro-left">
+        <h1>Bienvenue</h1>
+        <h2>Portfolio 2025</h2>
+      </div>
+      <div className="intro-right">
+        <h6>
+        Ici vous êtes dans <strong>mon univers</strong>, je vais vous parler de ce que <strong>j’ai fait</strong>, ce que <strong>je fais</strong>, mais aussi qui <strong>je suis</strong>.
+        <br />
+        <br />
+        Vous pourrez y <strong>découvrir mes projets</strong>, sur <strong>mes compétences</strong> et sur <strong>mon expérience</strong>.
+        <br />
+        <br />
+        J'aime la <strong>création de solutions innovantes</strong>, <strong>être en équipe</strong> pour créer un projet :
+        <br />
+        dans <strong>une équipe passionnée, passionnante, inspirée et inspirante</strong>.
+        <br />
+        <br />
+        <em>"De l'idée sur papier au site en production."</em>
+        <br />
+        <br />
+        Je recherche <strong>une alternance</strong> ou bien <strong>ma première expérience</strong>, dans l'idéal une agence digitale.
+        <br />
+        <br />
+        N'hésitez pas à <strong>parcourir mon travail</strong> et à <strong>me contacter</strong> pour toute question ou opportunité !
+        </h6>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ src/components/PhotosSection.jsx
@@ -0,0 +1,16 @@
+import './PhotosSection.css';
+
+
+function PhotosSection() {
+  return (
+    <div className="photos-section">
+      <div className="photo-1"></div>
+      <div className="photo-2"></div>
+      <div className="photo-3"></div>
+      <div className="photo-4"></div>
+      <div className="photo-5"></div>
+    </div>
+  );
+}
+
+export default PhotosSection;
