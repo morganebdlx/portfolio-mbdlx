ea4001d04d7d77a48b408e38736053d221d563fd
Morgane Bourdillon <mlacroix8@gmail.com>
2025-05-28 16:23:05 +0200

    fine tuning

--- src/components/PhotosSection.jsx
+++ src/components/PhotosSection.jsx
@@ -1,14 +1,19 @@
 import './PhotosSection.css';
+import vintageClock from '../assets/images/vintage-clock.jpg';
+import pipistrello from '../assets/images/pipistrello.jpg';
+import myself from '../assets/images/me.jpg';
+import carsNessino from '../assets/images/cars-nessino.jpg';
+import workplace from '../assets/images/workplace.jpeg';
 
 
 function PhotosSection() {
   return (
     <div className="photos-section">
-      <div className="photo-1"></div>
-      <div className="photo-2"></div>
-      <div className="photo-3"></div>
-      <div className="photo-4"></div>
-      <div className="photo-5"></div>
+      <div className="photo-1"><img src={vintageClock} alt="Horloge vintage" /></div>
+      <div className="photo-2"><img src={pipistrello} alt="Lampe Pipistrello" /></div>
+      <div className="photo-3"><img src={myself} alt="Protagoniste" /></div>
+      <div className="photo-4"><img src={workplace} alt="workplace" /></div>
+      <div className="photo-5"><img src={carsNessino} alt="Lego et Lampe Nessino" /></div>
     </div>
   );
 }
